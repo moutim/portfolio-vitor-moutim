4108ccf17ea47e8b547bc5a6ea0d8c27d5b41527
Vitor Moutim <moutimg@gmail.com>
2024-05-10 10:26:08 -0300

    fix: fix erros links

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-
+  downloadCurriculum() {
+    const link = document.createElement('a');
+    link.setAttribute('target', '_blank');
+    link.setAttribute('href', '../../assets/VITOR_MOUTIM_CURRICULO.pdf'); // Substitua pelo caminho do seu currículo
+    link.setAttribute('download', 'VITOR_MOUTIM_CURRICULO.pdf');
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  }
 }
