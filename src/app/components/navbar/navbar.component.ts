6e595ac9b7dae4a44cb2f5054181556a3e5137fc
Vitor Moutim <moutimg@gmail.com>
2024-05-10 12:19:49 -0300

    style: create banner for mobile

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -11,7 +11,7 @@ export class NavbarComponent {
   downloadCurriculum() {
     const link = document.createElement('a');
     link.setAttribute('target', '_blank');
-    link.setAttribute('href', '../../assets/VITOR_MOUTIM_CURRICULO.pdf'); // Substitua pelo caminho do seu currículo
+    link.setAttribute('href', '../../assets/VITOR_MOUTIM_CURRICULO.pdf');
     link.setAttribute('download', 'VITOR_MOUTIM_CURRICULO.pdf');
     document.body.appendChild(link);
     link.click();
